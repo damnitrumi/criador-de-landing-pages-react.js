75dd17d41392a6fe4dca14be49cdb3853f2c9a28
Damn it Rumi <guidepaulasoares@hotmail.com>
2022-07-13 15:35:42 -0300

    Pasta App removida

--- src/templates/App/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { css } from "styled-components";
-
-export const Wrapper = styled.div`
-  ${({ theme }) => css`
-    background: ${theme.colors.secondaryBg};
-  `}
-`;
